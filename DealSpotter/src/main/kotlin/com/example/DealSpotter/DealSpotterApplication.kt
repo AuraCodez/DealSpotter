32ba8ce49c510af589686fae9f4c76dd447c15d3
Ryan <86922669+AuraCodez@users.noreply.github.com>
2024-03-01 17:01:44 -0500

    initial project setup
    - used spring initzilir to setup project

---
+++ DealSpotter/src/main/kotlin/com/example/DealSpotter/DealSpotterApplication.kt
@@ -0,0 +1,10 @@
+package com.example.DealSpotter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class DealSpotterApplication
+
+fun main(args: Array<String>) {
+    runApplication<DealSpotterApplication>(*args)
+}
---
+++ DealSpotter/src/test/kotlin/com/example/DealSpotter/DealSpotterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.DealSpotter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DealSpotterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
